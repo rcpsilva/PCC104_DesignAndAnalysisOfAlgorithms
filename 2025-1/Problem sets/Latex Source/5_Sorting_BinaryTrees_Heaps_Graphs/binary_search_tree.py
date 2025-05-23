6a44fe318af263153e3bfc4790f06970caa4a71c
rcpsilva <rcpsilva@gmail.com>
2025-05-23 00:29:52 -0300

    Update binary_search_tree.py

--- 2025-1/Problem sets/Latex Source/5_Sorting_BinaryTrees_Heaps_Graphs/binary_search_tree.py
+++ 2025-1/Problem sets/Latex Source/5_Sorting_BinaryTrees_Heaps_Graphs/binary_search_tree.py
@@ -21,7 +21,23 @@ class BST():
         Args:
             value (int): Valor a ser inserido na árvore.
         """
-        pass
+        if not self.root:
+            self.root = Node(value)
+        else:
+            self._insert(self.root,value)
+
+    def _insert(self, node, value):
+
+        if value < node.value:
+            if not node.left:
+                node.left = Node(value)
+            else:
+                self._insert(node.left,value)
+        elif value > node.value:
+            if not node.right:
+                node.right = Node(value)
+            else:
+                self._insert(node.right,value)        
 
     def remove(self, value):
         """Remove um valor da Árvore Binária de Busca, se presente.
@@ -48,7 +64,18 @@ class BST():
         Returns:
             int: A altura da árvore (maior número de arestas entre a raiz e uma folha).
         """
-        pass
+
+        if not self.root:
+            return 0
+        else:
+            return self._height(self.root)
+        
+    def _height(self,node):
+        
+        if not node:
+            return 0
+        else:    
+            return max(self._height(node.left),self._height(node.right)) + 1
 
     def inorder(self):
         """Retorna uma lista com os valores da árvore em ordem crescente (inordem).
@@ -74,30 +101,40 @@ if __name__ == '__main__':
 
     tree = BST()
 
-    tree.insert(2)
-    tree.insert(4)
-    tree.insert(1)
-    tree.insert(9)
-    tree.insert(8)
-    tree.insert(7)
-    tree.insert(3)
     tree.insert(5)
-    tree.insert(6)
+    tree.insert(4)
+    tree.insert(15)
+    tree.insert(12)
+    tree.insert(16)
+    tree.insert(21)
+    tree.insert(11)
+    tree.insert(10)
+    tree.insert(9)
 
-    print(tree.inorder())
+    #tree.insert(2)
+    #tree.insert(4)
+    #tree.insert(1)
+    #tree.insert(9)
+    #tree.insert(8)
+    #tree.insert(7)
+    #tree.insert(3)
+    #tree.insert(5)
+    #tree.insert(6)
 
-    print(tree.search(1))
-    print(tree.search(2))
-    print(tree.search(3))
-    print(tree.search(4))
-    print(tree.search(5))
-    print(tree.search(6))
-    print(tree.search(7))
-    print(tree.search(8))
-    print(tree.search(9))
+    #print(tree.inorder())
 
-    print(tree.search(10))
-    print(tree.search(11))
+    #print(tree.search(1))
+    #print(tree.search(2))
+    #print(tree.search(3))
+    #print(tree.search(4))
+    #print(tree.search(5))
+    #print(tree.search(6))
+    #print(tree.search(7))
+    #print(tree.search(8))
+    #print(tree.search(9))
+
+    #print(tree.search(10))
+    #print(tree.search(11))
 
     print(f'height {tree.height()}')
 
