43e1b2a46e247fe318a300719cf23db79214cc6b
rcpsilva <rcpsilva@gmail.com>
2026-02-18 22:42:47 -0300

    Create cronograma.mk

---
+++ 2026-1/cronograma.mk
@@ -0,0 +1,38 @@
+ Semana     & Conteúdo                                          	
+ ---------- &---------------------------------------------------	
+20/03	& Início das Aulas - Análise e Projeto de Algoritmos
+27/03	& Caracterização dos tempos de execução
+03/04	& (Feriado) Sexta-Feira Santa
+10/04	& Divisão e Conquista e Analise Probabilística
+17/04	& Prova 1
+24/04	& Heaps, Heapsort e QuickSort
+01/05	& (Feriado) Dia do Trabalhador
+08/05	& Ordenação em Tempo Linear e Estatísticas de Ordem
+15/05	& Prova 2
+22/05	& Estruturas de Dados Elementares e Tabelas de Espalhamento
+29/05	& (Feriado) Corpus Christi
+05/06	& Árvores Binárias de Busca e Árvores Rubro-Negras
+12/06	& Programação Dinâmica e Algoritmos Gulosos
+19/06	& Prova 3
+26/06	& Algoritmos Elementares em Grafos e Árvores Geradoras Mínimas
+03/07	& Caminhos Mínimos e Fluxo Máximo
+10/07	& Problemas NP-Completos
+17/07	& Prova 4
+ 
+Metodologia:
+
+- Aulas expositivas sobre o conteúdo programático.
+
+- Seminários com professores e profissionais convidados.
+
+- Provas: Testes individuais feitos durante as aulas.
+
+- Leituras recomendadas: leitura de textos técnicos com a finalidade de proporcionar ao discente a oportunidade de consulta e desenvolvimento de sua capacidade de análise, síntese e crítica de uma bibliografia específica.
+
+Exame Especial: Os alunos que tiverem pelo menos 75% de frequência (mínimo para aprovação) e média inferior a seis pontos poderão fazer o Exame Especial ou o Exame Especial Parcial. Estes exames serão provas únicas, individuais.
+
+Atividades avaliativas:
+
+- 4 provas (P1, P2, P3, P4) no valor de 10 pontos.
+
+Nota final = 0.15 * P1 + 0.2 * P2 + 0.2 * P3 + 0.45 * P4
