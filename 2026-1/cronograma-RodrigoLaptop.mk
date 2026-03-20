1b0448d3882b14c5bf577914ace7c332ec621f53
rcpsilva <rcpsilva@gmail.com>
2026-03-20 14:05:42 -0300

    Create cronograma-RodrigoLaptop.mk

---
+++ 2026-1/cronograma-RodrigoLaptop.mk
@@ -0,0 +1,56 @@
+ Data    & Conteúdo                                          	
+ ---------- &---------------------------------------------------	
+20/03	& Início das Aulas - Análise e Projeto de Algoritmos
+27/03	& Caracterização dos tempos de execução
+03/04	& (Feriado) Sexta-Feira Santa
+10/04	& Divisão e Conquista e Analise Probabilística
+17/04	& Prova 1
+24/04	& Heaps, Heapsort e QuickSort
+01/05	& (Feriado) Dia do Trabalhador
+08/05	& Ordenação em Tempo Linear e Estatísticas de Ordem
+15/05	& Prova 2
+22/05	& Estruturas de Dados Elementares e Tabelas de Espalhamento
+29/05	& (Feriado) Corpus Christi
+05/06	& Árvores Binárias de Busca e Árvores Rubro-Negras
+12/06	& Programação Dinâmica e Algoritmos Gulosos
+19/06	& Prova 3
+26/06	& Algoritmos Elementares em Grafos e Árvores Geradoras Mínimas
+03/07	& Caminhos Mínimos e Fluxo Máximo
+10/07	& Problemas NP-Completos
+17/07	& Prova 4
+ 
+Metodologia:
+
+- Aulas expositivas sobre o conteúdo programático.
+
+- Provas: Testes individuais feitos durante as aulas.
+
+- Leituras recomendadas: leitura de textos técnicos com a finalidade de proporcionar ao discente a oportunidade de consulta e desenvolvimento de sua capacidade de análise, síntese e crítica de uma bibliografia específica.
+
+Atividades avaliativas:
+
+- 4 provas (P1, P2, P3, P4) no valor de 10 pontos.
+
+Nota final = 0.15 * P1 + 0.2 * P2 + 0.2 * P3 + 0.45 * P4
+
+
+Bibliografia principal:
+
+CORMEN, Thomas H.; LEISERSON, Charles E.; RIVEST, Ronald L.; STEIN, Clifford. Algoritmos: teoria e prática. 4. ed. Rio de Janeiro: Grupo GEN | LTC, 2022.  https://integrada.minhabiblioteca.com.br/books/9788595159914
+. Acesso em: 20 fev. 2026.
+
+LEVITIN, Anany. Introduction to Design and Analysis of Algorithms. 3. ed. Boston: Pearson, 2012.
+
+SZWARCFITER, Jayme Luiz; MARKENZON, Lilian. Estruturas de dados e seus algoritmos. 3. ed. Rio de Janeiro: Grupo GEN, 2010. 
+
+
+Bibliografia complementar:
+
+SEDGEWICK, Robert. Algorithms. 2. ed. Reading: Addison-Wesley, 1988. ISBN 0201066734.
+
+ZIVIANI, Nivio. Projeto de algoritmos com implementações em Java e C++. São Paulo: Cengage Learning, 2006. ISBN 8522105251.
+
+LAMBERT, Kenneth A. Fundamentos de Python: estruturas de dados. São Paulo: Cengage Learning Brasil, 2022. Disponível em: Minha Biblioteca. Acesso em: 20 fev. 2026.
+
+WAZLAWICK, Raul Sidnei. Introdução a algoritmos e programação com Python: uma abordagem dirigida por testes. Rio de Janeiro: Grupo GEN, 2018. Disponível em: https://integrada.minhabiblioteca.com.br/books/9788595156968
+. Acesso em: 20 fev. 2026.
